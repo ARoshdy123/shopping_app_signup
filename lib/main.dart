40d3bd3233ceeb75b517f7df828b12fd62c39595
Ahmed Roshdy Ragheb Mohamed <ahmedroshdy6555554@gmail.com>
2025-01-04 23:53:28 +0200

    commit for adding  localization

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,21 @@
+import 'package:easy_localization/easy_localization.dart';
 import 'package:flutter/material.dart';
 import 'package:shopping_app_signup/screens/signup.dart';
 import 'package:shopping_app_signup/widgets/display_product.dart';
 import 'package:shopping_app_signup/widgets/grid_view.dart';
 import 'package:shopping_app_signup/widgets/hot_offers.dart';
 
-void main() {
-  runApp(const ShoppingApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await EasyLocalization.ensureInitialized();
+  runApp(
+    EasyLocalization(
+        supportedLocales: [Locale('en', 'US'), Locale('ar', 'EG')],
+        path: 'languages', // <-- change the path of the translation files
+        fallbackLocale: Locale('en', 'US'),
+        child: ShoppingApp()),
+  );
+  // runApp(const ShoppingApp());
 }
 
 class ShoppingApp extends StatelessWidget {
@@ -13,8 +23,11 @@ class ShoppingApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: SignUpScreen(),
+    return MaterialApp(
+      localizationsDelegates: context.localizationDelegates,
+      supportedLocales: context.supportedLocales,
+      locale: context.locale,
+      home: const SignUpScreen(),
     );
   }
 }
@@ -31,7 +44,19 @@ class _MainScreenState extends State<MainScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Shopping App'),
+        title: Text('shopping_app_title'.tr()),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.language),
+            onPressed: () {
+              if (context.locale == Locale('en', 'US')) {
+                context.setLocale(Locale('ar', 'EG'));
+              } else {
+                context.setLocale(Locale('en', 'US'));
+              }
+            },
+          ),
+        ],
       ),
       body: SingleChildScrollView(
         scrollDirection: Axis.vertical,
@@ -41,8 +66,8 @@ class _MainScreenState extends State<MainScreen> {
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-              const Text(
-                "Our Products",
+              Text(
+                'our_products'.tr(),
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 10),
@@ -50,8 +75,8 @@ class _MainScreenState extends State<MainScreen> {
               const SizedBox(height: 20),
               GridViewProduct(),
               const SizedBox(height: 20),
-              const Text(
-                "Hot Offers",
+              Text(
+                'hot_offers'.tr(),
                 style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
               ),
               const SizedBox(height: 10),
